bb0747c1af7f5f8080135525509231f21ee4a984
davalcato <davalcato@yahoo.com>
2023-04-10 16:27:53 -0700

    Made the Email button led to an email contact list

--- CarDryver/Screens/SignInScreenView.swift
+++ CarDryver/Screens/SignInScreenView.swift
@@ -10,9 +10,10 @@ import Firebase
 import GoogleSignIn
 
 struct SignInScreenView: View {
-    @State private var email: String = "" // by default it's empty
+    @State private var email: String = ""
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
-
+    @State private var showAlert = false
+    
     var body: some View {
         ZStack {
             Color("BgColor").edgesIgnoringSafeArea(.all)
@@ -22,56 +23,31 @@ struct SignInScreenView: View {
                 }
                 .padding(.horizontal)
                 Spacer()
-
+                
                 VStack {
-                    NavigationLink(destination: WelcomeScreenView()) {
-                        Text("Sign In")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                    }
-                    .padding(.bottom, 30)
-
                     Button(action: {
-                        // Sign in with Apple button pressed
                         print("Sign in with Apple button pressed")
                     }) {
-                        HStack {
-                            Image(uiImage: #imageLiteral(resourceName: "apple"))
-                                .resizable()
-                                .frame(width: 20, height: 20)
-                            Text("Sign in with Apple")
-                                .foregroundColor(Color("PrimaryColor"))
-                        }
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.white)
-                        .cornerRadius(25)
-                        .padding(.vertical)
+                        SocalLoginButton(image: Image(uiImage: #imageLiteral(resourceName: "apple")), text: Text("Sign in with Apple").foregroundColor(Color("PrimaryColor")))
+                            .padding(.vertical, 20)
+                            .background(Color.white)
+                            .cornerRadius(50)
+                            .frame(width: 300)
                     }
-                    .buttonStyle(BorderlessButtonStyle())
-
+                    
                     Button(action: {
-                        // Sign in with Google button pressed
                         print("Sign in with Google button pressed")
                     }) {
-                        HStack {
-                            Image(uiImage: #imageLiteral(resourceName: "google"))
-                                .resizable()
-                                .frame(width: 20, height: 20)
-                            Text("Sign in with Google")
-                                .foregroundColor(Color("PrimaryColor"))
-                        }
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.white)
-                        .cornerRadius(25)
-                        .padding(.vertical)
+                        SocalLoginButton(image: Image(uiImage: #imageLiteral(resourceName: "google")), text: Text("Sign in with Google").foregroundColor(Color("PrimaryColor")))
+                            .padding(.vertical, 20)
+                            .background(Color.white)
+                            .cornerRadius(50)
+                            .frame(width: 300)
                     }
-                    .buttonStyle(BorderlessButtonStyle())
-
+                    
                     Text("or get a link emailed to you")
                         .foregroundColor(Color.black.opacity(0.4))
-
+                    
                     TextField("Work email address", text: $email)
                         .font(.title3)
                         .padding()
@@ -80,11 +56,27 @@ struct SignInScreenView: View {
                         .cornerRadius(50.0)
                         .shadow(color: Color.black.opacity(0.18), radius: 60, x: 0.0, y: 16)
                         .padding(.vertical)
-
-                    PrimaryButton(title: "Email me a signup link")
-
+                    
+                    Button(action: {
+                        print("Email me a signup link button pressed")
+                        showAlert = true
+                    }) {
+                        PrimaryButton(title: "Email me a signup link")
+                    }
+                    .alert(isPresented: $showAlert, content: {
+                        Alert(
+                            title: Text("Contact"),
+                            message: Text("Do you want to contact us via email?"),
+                            primaryButton: .default(Text("Email"), action: {
+                                if let url = URL(string: "mailto:support@yourapp.com") {
+                                    UIApplication.shared.open(url)
+                                }
+                            }),
+                            secondaryButton: .cancel()
+                        )
+                    })
                 }
-
+                
                 Spacer()
                 Divider()
                 Spacer()
@@ -92,7 +84,7 @@ struct SignInScreenView: View {
                 Text("Read our Terms & Conditions.")
                     .foregroundColor(Color("PrimaryColor"))
                 Spacer()
-
+                
             }
             .padding()
         }
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '10.0'
+
+target 'CarDryver' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+
+
+  # Pods for CarDryver
+
+  target 'CarDryverTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CarDryverUITests' do
+    # Pods for testing
+  end
+
+end
