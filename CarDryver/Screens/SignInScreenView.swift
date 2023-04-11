866d1b0be8de705cbfece92796486f5e4f8b2ef3
davalcato <davalcato@yahoo.com>
2023-04-11 12:18:04 -0700

    Updated file

--- CarDryver/Screens/SignInScreenView.swift
+++ CarDryver/Screens/SignInScreenView.swift
@@ -15,95 +15,91 @@ struct SignInScreenView: View {
     @State private var showAlert = false
     
     var body: some View {
-        NavigationView {
-            ZStack {
-                Color("BgColor").edgesIgnoringSafeArea(.all)
+        ZStack {
+            Color("BgColor").edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack {
+                    Spacer()
+                }
+                .padding(.horizontal)
+                Spacer()
+                
                 VStack {
-                    HStack {
-                        Spacer()
-                    }
-                    .padding(.horizontal)
-                    Spacer()
-                    
-                    VStack {
-                        Button(action: {
-                            print("Sign in with Apple button pressed")
-                        }) {
-                            SocalLoginButton(image: Image(uiImage: #imageLiteral(resourceName: "apple")), text: Text("Sign in with Apple").foregroundColor(Color("PrimaryColor")))
-                                .padding(.vertical, 20)
-                                .background(Color.white)
-                                .cornerRadius(50)
-                                .frame(width: 300)
-                        }
-                        
-                        Button(action: {
-                            print("Sign in with Google button pressed")
-                        }) {
-                            SocalLoginButton(image: Image(uiImage: #imageLiteral(resourceName: "google")), text: Text("Sign in with Google").foregroundColor(Color("PrimaryColor")))
-                                .padding(.vertical, 20)
-                                .background(Color.white)
-                                .cornerRadius(50)
-                                .frame(width: 300)
-                        }
-                        
-                        Text("or get a link emailed to you")
-                            .foregroundColor(Color.black.opacity(0.4))
-                        
-                        TextField("Work email address", text: $email)
-                            .font(.title3)
-                            .padding()
-                            .frame(maxWidth: .infinity)
+                    Button(action: {
+                        print("Sign in with Apple button pressed")
+                    }) {
+                        SocalLoginButton(image: Image(uiImage: #imageLiteral(resourceName: "apple")), text: Text("Sign in with Apple").foregroundColor(Color("PrimaryColor")))
+                            .padding(.vertical, 20)
                             .background(Color.white)
-                            .cornerRadius(50.0)
-                            .shadow(color: Color.black.opacity(0.18), radius: 60, x: 0.0, y: 16)
-                            .padding(.vertical)
-                        
-                        Button(action: {
-                            print("Email me a signup link button pressed")
-                            showAlert = true
-                        }) {
-                            PrimaryButton(title: "Email me a signup link")
-                        }
-                        .alert(isPresented: $showAlert, content: {
-                            Alert(
-                                title: Text("Contact"),
-                                message: Text("Do you want to contact us via email?"),
-                                primaryButton: .default(Text("Email"), action: {
-                                    if let url = URL(string: "mailto:support@yourapp.com") {
-                                        UIApplication.shared.open(url)
-                                    }
-                                }),
-                                secondaryButton: .cancel()
-                            )
-                        })
+                            .cornerRadius(50)
+                            .frame(width: 300)
                     }
                     
-                    Spacer()
-                    Divider()
-                    Spacer()
-                    Text("You are completely safe.")
-                    
-                    NavigationLink(destination: TermsAndConditionsView()) {
-                        Text("Read our Terms & Conditions.")
-                            .bold()
-                            .foregroundColor(Color("PrimaryColor"))
-                            .font(.system(size: 13))
+                    Button(action: {
+                        print("Sign in with Google button pressed")
+                    }) {
+                        SocalLoginButton(image: Image(uiImage: #imageLiteral(resourceName: "google")), text: Text("Sign in with Google").foregroundColor(Color("PrimaryColor")))
+                            .padding(.vertical, 20)
+                            .background(Color.white)
+                            .cornerRadius(50)
+                            .frame(width: 300)
                     }
                     
-                    Spacer()
+                    Text("or get a link emailed to you")
+                        .foregroundColor(Color.black.opacity(0.4))
                     
+                    TextField("Work email address", text: $email)
+                        .font(.title3)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.white)
+                        .cornerRadius(50.0)
+                        .shadow(color: Color.black.opacity(0.18), radius: 60, x: 0.0, y: 16)
+                        .padding(.vertical)
+                    
+                    Button(action: {
+                        print("Email me a signup link button pressed")
+                        showAlert = true
+                    }) {
+                        PrimaryButton(title: "Email me a signup link")
+                    }
+                    .alert(isPresented: $showAlert, content: {
+                        Alert(
+                            title: Text("Contact"),
+                            message: Text("Do you want to contact us via email?"),
+                            primaryButton: .default(Text("Email"), action: {
+                                if let url = URL(string: "mailto:support@yourapp.com") {
+                                    UIApplication.shared.open(url)
+                                }
+                            }),
+                            secondaryButton: .cancel()
+                        )
+                    })
                 }
-                .padding()
+                
+                Spacer()
+                Divider()
+                Spacer()
+                Text("You are completely safe.")
+                
+                NavigationLink(destination: TermsAndConditionsView()) {
+                    Text("Read our Terms & Conditions.")
+                        .bold()
+                        .foregroundColor(Color("PrimaryColor"))
+                        .font(.system(size: 13))
+                }
+                
+                Spacer()
+                
             }
-            .gesture(DragGesture().onEnded({ (value) in
-                if value.translation.width > 100 {
-                    self.presentationMode.wrappedValue.dismiss()
-                }
-            }))
-            .navigationTitle("Sign In")
+            .padding()
         }
+        .gesture(DragGesture().onEnded({ (value) in
+            if value.translation.width > 100 {
+                self.presentationMode.wrappedValue.dismiss()
+            }
+        }))
     }
-}
 
 struct TermsAndConditionsView: View {
     var body: some View {
@@ -139,4 +135,5 @@ struct SocalLoginButton: View {
         .cornerRadius(50.0)
         .shadow(color: Color.black.opacity(0.08), radius: 60, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 16)
     }
+}
 }
